fcdf81b1bf5e5e9c559c3c702fdf813d14362fdc
nisreendh05 <your-email@example.com>
2026-09-05 03:35:52 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const btnEl = document.querySelector(".btn")
+btnEl.addEventListener("click", ()=>{
+        creatPassword() 
+})
+
+function creatPassword(){
+        const chars = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        const passwordLength = 14;
+        let password =""
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            
+        }
+}
